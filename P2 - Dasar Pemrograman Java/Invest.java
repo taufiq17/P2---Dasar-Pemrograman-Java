5af1ee2e12b2bef1544b034fc029e6fac0cf37e8
taufiq17 <32066322+taufiq17@users.noreply.github.com>
2017-10-05 21:26:11 +0700

    Add files via upload

---
+++ P2 - Dasar Pemrograman Java/Invest.java
@@ -0,0 +1,14 @@
+public class Invest {
+    public static void main (String[] arguments) {
+        float total = 14000;
+        System.out.println("Original investment: $" + total);
+
+        //Increases by 40 percent the first year total = total + (total * .4F);
+        System.out.println("After one year: $" + total);
+        //Loses $1,500 the second year total = total - 1500F;
+        System.out.println("After two years: $" + total);
+
+        //Increases by 12 percent the third year total = total + (total * .12F);
+        System.out.println("After three years: $" + total);
+    }
+}
